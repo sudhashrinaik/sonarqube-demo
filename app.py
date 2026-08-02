fb95283a5888d8014a2d711e4251d6664f9b8ca4
Sudhashri Rajendra Naik <55624217+sudhashrinaik@users.noreply.github.com>
2026-08-02 19:13:29 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import os
+
+password = "admin123"
+
+def calculate(a,b):
+
+    if a>0:
+        print("Positive")
+
+    if a>0:
+        print("Positive")
+
+    return a+b
+
+
+def unused_function():
+    x=10
+    y=20
+
+
+print(calculate(5,6))
